3d193544c7c8780092f47af7265253a44612964e
Luis Fernando <fernando.chamorro@onetree.com>
2023-05-21 21:18:21 -0500

    Created manual basic configuration about Router Module (SPA)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [AppRoutingModule, BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
