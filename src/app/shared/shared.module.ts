b6798bb9a6239413258e58f043d64c6ca0d3d6a0
Luis Fernando <fernando.chamorro@onetree.com>
2023-05-21 21:47:28 -0500

    Added new option menu for load ContactPageComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,23 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AboutPageComponent, HomePageComponent, SidebarComponent],
+  declarations: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [AboutPageComponent, HomePageComponent, SidebarComponent],
+  exports: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+  ],
 })
 export class SharedModule {}
