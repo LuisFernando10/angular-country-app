d670e4a3f1aac62d08467bde721f26d2b856510f
Luis Fernando <fernando.chamorro@onetree.com>
2023-06-26 21:29:38 -0500

    Implemented view to see the country data on page

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styles: [],
 })
 export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
   constructor(
     private _activatedRoute: ActivatedRoute,
     private _countriesService: CountriesService,
@@ -22,10 +25,9 @@ export class CountryPageComponent implements OnInit {
         switchMap(({ id }) => this._countriesService.searchCountryByCode(id))
       )
       .subscribe((country) => {
-        if (!country) return this._router.navigate(['']);
+        if (!country) return this._router.navigateByUrl('');
 
-        console.log(country);
-        return;
+        return (this.country = country);
       });
   }
 }
