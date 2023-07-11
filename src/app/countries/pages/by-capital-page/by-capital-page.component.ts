3575584e0a23e440baf1abc29103f9a3e8b73760
Luis Fernando <fernando.chamorro@onetree.com>
2023-07-10 21:28:43 -0500

    Implemented loading logic

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -11,10 +11,14 @@ export class ByCapitalPageComponent {
   constructor(private _countriesService: CountriesService) {}
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   searchByCapital(capital: string): void {
+    this.isLoading = true;
+
     this._countriesService.searchCapital(capital).subscribe((countries) => {
       this.countries = countries;
+      this.isLoading = false;
     });
   }
 }
