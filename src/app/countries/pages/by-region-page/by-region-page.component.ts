4bc04a0bad4f552fa414788f20735f5f39933b46
Luis Fernando <fernando.chamorro@onetree.com>
2023-07-12 21:11:50 -0500

    Implemented Regions type array to selected them with button in byRegion page

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
+import { Regions } from '../types/by-region';
 
 @Component({
   selector: 'app-by-region-page',
@@ -12,9 +13,18 @@ export class ByRegionPageComponent {
 
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public selectedRegion?: Regions;
+  public regions: Regions[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
 
-  searchByRegion(region: string): void {
+  searchByRegion(region: Regions): void {
     this.isLoading = true;
+    this.selectedRegion = region;
 
     this._countriesService.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
